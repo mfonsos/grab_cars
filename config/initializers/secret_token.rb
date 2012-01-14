fadcc85154e741841f7de642faac568118bdd570
mfonsos <mfonsos@gmail.com>
2012-01-14 18:35:22 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrabCars::Application.config.secret_token = '2e529a6fb44da8c2f09db6ae26b16b24bb022002b7e87a2087ba7aaca7745866336fbf7fc7fa6b49738f40af2108b8d50bb9ced9a2c8956d6bb10abae93a7443'
